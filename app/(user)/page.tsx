3626843a689dcf0b071f8f07d5e192f0f2667d0c
cafeblanc <mrwellbeing44@gmail.com>
2026-03-27 10:43:09 +0900

    page.tsx 업데이트

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,3 +1,51 @@
-export default function AdminPage() {
-  return <div>관리자 대시보드</div>
+"use client"
+
+import { useState } from "react"
+import { supabase } from "@/lib/supabaseClient"
+import { useRouter } from "next/navigation"
+
+export default function SignupPage() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const router = useRouter()
+
+  const handleSignup = async () => {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    })
+
+    if (error) {
+      alert(error.message)
+      return
+    }
+
+    alert("회원가입 완료")
+    router.push("/login")
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>회원가입</h2>
+
+      <input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleSignup}>가입</button>
+    </div>
+  )
 }
